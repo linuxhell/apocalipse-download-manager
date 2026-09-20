a6e06be0221396f8078b07cf76c8958175f02c04
linuxhell <38770145+linuxhell@users.noreply.github.com>
2026-09-20 14:23:06 -0300

    Fix enlarged Settings regression matcher

--- tests/link-folder-settings.test.cjs
+++ tests/link-folder-settings.test.cjs
@@ -61,7 +61,8 @@ test("Linux Samba shares are discovered from smb.conf and usershares", () => {
 });
 
 test("Link lists remain readable and settings use the available window", () => {
-  assert.match(css, /#settings-dialog \\{ width: min\\(1480px, calc\\(100vw - 16px\\)\\)/);
+  assert.match(css, /#settings-dialog \{ width: min\(1480px, calc\(100vw - 16px\)\)/);
+  assert.match(css, /height: min\(960px, calc\(100vh - 16px\)\)/);
   assert.match(css, /height:clamp\(300px,42vh,460px\)/);
   assert.match(css, /text-overflow:ellipsis/);
 });
